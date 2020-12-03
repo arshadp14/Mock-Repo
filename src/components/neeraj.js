336ff53a6f077fee8777a7b60a52c1a81b732cfe
nirajgo <neerajgo@cybage.com>
2020-12-03 19:47:18 +0530

    neeraj modified component

--- src/components/neeraj.js
+++ src/components/neeraj.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import './Home.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import { Container } from 'react-bootstrap'
+import React from 'react';
+import './Home.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container } from 'react-bootstrap';
 
-export class Home extends React.Component{
-    render(){
-        return(
-            <Container>
-               Hello guys
-            </Container>
-        )
-    }
+export class Home extends React.Component {
+	render() {
+		return (
+			<Container>
+				Hello guys , Your in Neeraj's Component.!!! Have a nice day!
+			</Container>
+		);
+	}
 }
