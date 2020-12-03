e1f29d3e31f512d1d34ce83d2ed6cf485b6d3e1b
Arshad <arshadp@cybage.com>
2020-12-03 15:01:03 +0530

    home component added

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Container } from 'react-bootstrap'
+
+export class Home extends React.Component{
+    render(){
+        return(
+            <Container>
+               Hello guys
+            </Container>
+        )
+    }
+}
